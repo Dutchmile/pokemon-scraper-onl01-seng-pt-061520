c1dd79496149ea4d4c8acc2e1e03373ee8b3faad
Christopher Dutcher <cadutcher@icloud.com>
2020-10-13 20:44:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,7 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
   
   
-  def initialize(id:, name:, type:, db:)
+  def initialize(id: = nil, name:, type:, db:)
       @id = id
       @name = name
       @type = type
@@ -13,9 +13,10 @@ class Pokemon
     end
   
   def self.save(name, type, db)
+    new_pokemon = self.new(name, type, db)
     sql = <<-SQL
     INSERT INTO pokemon (name, type) VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, self.name, self.type)  
+    DB[:conn].execute(sql, new_pokemon.name, new_pokemon.type)  
   end
 end
